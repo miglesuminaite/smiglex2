1b56dc362754cd4431b9d0c5d2d1a1df15006eb3
miglesuminaite <159789395+miglesuminaite@users.noreply.github.com>
2024-02-12 17:52:49 +0200

    Update main.py
    Parasiau likusi koda:

--- main.py
+++ main.py
@@ -29,3 +29,32 @@ def funkcija ():
     print("[1] Atimtis")
     print("[1] Daugyba")
     print("[1] Dalyba")
+
+while True:
+    choice = input("Iveskite pasirinkima(1/2/3/4): ")
+
+    if choice in ('1', '2', '3', '4'):
+        try:
+            num1 = float(input("Iveskite pirma skaiciu: "))
+            num2 = float(input("Iveskite antra skaiciu: "))
+        except ValueError:
+            print("Blogai. Bandykite dar karta.")
+            continue
+
+        if choice == '1':
+            print(num1, "+", num2, "=", add(num1, num2))
+
+        elif choice == '2':
+            print(num1, "-", num2, "=", subtract(num1, num2))
+
+        elif choice == '3':
+            print(num1, "*", num2, "=", multiply(num1, num2))
+
+        elif choice == '4':
+            print(num1, "/", num2, "=", divide(num1, num2))
+        
+        next_calculation = input("Skaiciuojam dar karta? (taip/ne): ")
+        if next_calculation == "ne":
+          break
+    else:
+        print("Error. Bandom dar karta.")
