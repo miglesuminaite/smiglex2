ba2f40d3d99cb33315640ead0096e623e6b5ccca
miglesuminaite <159789395+miglesuminaite@users.noreply.github.com>
2024-02-12 16:31:13 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+
+
+
+kintamasis = 1
+kintamasis2 = 2
+
+def funkcija ():
+    print(kintamasis)
+    print(kintamasis2)
+    print(f'test:{kintamasis2}{kintamasis}')
+    print('test:{}{}'.format(kintamasis2, kintamasis))
